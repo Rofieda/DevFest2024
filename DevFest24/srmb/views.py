8afd95c17634f31a58db026f80f7c3e26ed41ff1
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-10-19 11:15:00 +0100

    Update views.py

--- DevFest24/srmb/views.py
+++ DevFest24/srmb/views.py
@@ -1,15 +1,9 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-<<<<<<< HEAD
-from .models import Depenses ,Entreprise
+from .models import Depenses ,Entreprise, Revenue
 from .serializers import DepensesSerializer ,RevenueSerializer, UtilisateurSerializer
-=======
-from .models import Depenses ,Entreprise , Revenue
-from .serializers import DepensesSerializer ,RevenueSerializer
 from rest_framework import generics
->>>>>>> 8bfe9c25e17f84a5955e2122e28256257a19cc3a
-
 from .serializers import EntrepriseSerializer
 
 class EntrepriseCreateView(APIView):
@@ -36,7 +30,16 @@ class AddRevenue(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-<<<<<<< HEAD
+    class DepensesListAPIView(generics.ListAPIView):
+     queryset = Depenses.objects.all()
+     serializer_class = DepensesSerializer
+
+class RevenuesListView(APIView):
+    def get(self, request):
+        revenues = Revenue.objects.all()
+        serializer = RevenueSerializer(revenues, many=True)
+        return Response(serializer.data)
+    
 class AddUtilisateur(APIView):
     def post(self, request):
         serializer = UtilisateurSerializer(data=request.data)
@@ -44,16 +47,3 @@ class AddUtilisateur(APIView):
             serializer.save()  # Save the valid data into the Utilisateur model
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
-
-class DepensesListAPIView(generics.ListAPIView):
-    queryset = Depenses.objects.all()
-    serializer_class = DepensesSerializer
-
-
-class RevenuesListView(APIView):
-    def get(self, request):
-        revenues = Revenue.objects.all()
-        serializer = RevenueSerializer(revenues, many=True)
-        return Response(serializer.data)
->>>>>>> 8bfe9c25e17f84a5955e2122e28256257a19cc3a
